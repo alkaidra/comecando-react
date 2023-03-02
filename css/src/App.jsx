a119e2009e0d3c57f8cd219bbc313c2b922e52bf
alkaidra <joaovitoralcantarasilva44@gmail.com>
2023-03-02 11:16:12 -0300

    comecando com formularios, terminando a sessão css

---
+++ css/src/App.jsx
@@ -0,0 +1,13 @@
+import Teste from './components/Teste';
+
+import './App.css';
+
+function App() {
+    return (
+      <div className='App'>
+          <Teste />
+      </div>
+    );
+}
+
+export default App;
